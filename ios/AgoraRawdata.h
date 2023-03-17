881f0aa0b9859811cb16d203b561611716a631ee
Arthas <luxuhui@agora.io>
2023-03-17 16:05:50 +0800

    Update AgoraRawdata.h

--- ios/AgoraRawdata.h
+++ ios/AgoraRawdata.h
@@ -2,14 +2,8 @@
 #import "plugin_c_api.h"
 #endif
 
-#ifdef RCT_NEW_ARCH_ENABLED
-#import "RNAgoraRawdataSpec.h"
-
-@interface AgoraRawdata : NSObject <NativeAgoraRawdataSpec>
-#else
 #import <React/RCTBridgeModule.h>
 
 @interface AgoraRawdata : NSObject <RCTBridgeModule>
-#endif
 
 @end
